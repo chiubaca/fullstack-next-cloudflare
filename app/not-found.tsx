655ffb12a3380eecd7e4f2d4a28f9fb958cbe254
Alex Chiu <alexander.chiu@houseful.co.uk>
2024-04-21 15:09:09 +0100

    feat: setup clerk auth

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -20,7 +20,7 @@ export default function NotFound() {
         </div>
       </div>
     </>
-  );
+  )
 }
 
 const styles = {
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
+"use client";
+
+import { useAuth, useClerk, UserButton } from "@clerk/nextjs";
+// import { Authenticated, Unauthenticated, AuthLoading } from "convex/react";
 import Image from "next/image";
 
 export default function Home() {
+  const clerk = useClerk();
+
+  const { isSignedIn, isLoaded } = useAuth();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -9,34 +17,22 @@ export default function Home() {
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          {isSignedIn && <UserButton />}
+
+          {!isSignedIn && (
+            <button
+              type="button"
+              className="bg-slate-200 rounded-full p-2 btn"
+              onClick={() => clerk.openSignIn({})}
+            >
+              Log in
+            </button>
+          )}
         </div>
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+        hello!
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
