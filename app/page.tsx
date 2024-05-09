0dad1468194a887c4ebb9c73163f2cc2ab77a385
Alex Chiu <alexchiu11@gmail.com>
2024-05-09 15:59:10 +0100

    testing..

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,14 @@ import { drizzle } from "drizzle-orm/d1";
 import { revalidatePath } from "next/cache";
 import { getRequestContext } from "@cloudflare/next-on-pages";
 
-import { auth } from "@/auth";
 import { todo } from "@/schemas";
-import { SignInButton } from "@/components/sign-in";
 
 export const runtime = "edge";
 
+const APP_DB = getRequestContext().env.APP_DB;
+const db = drizzle(APP_DB);
+
 export default async function Home() {
-  const APP_DB = getRequestContext().env.APP_DB;
-  const db = drizzle(APP_DB);
-
-  const session = await auth();
-
   const todos = await db.select().from(todo);
 
   async function addTodo(formData: FormData) {
@@ -23,27 +19,19 @@ export default async function Home() {
       const formTodo = formData.get("todo") as string;
 
       await db.insert(todo).values({ text: formTodo, timestamp: Date.now() });
-      // .returning();
-
-      revalidatePath("/posts");
     } catch (error) {
-      console.warn("Unhandled error!", error);
+      console.warn("Unhandled error...", error);
     }
   }
 
   return (
     <main>
-      <SignInButton />
-
-      {session ? <>Hello {session?.user?.name}</> : <> You are signed out</>}
-
       <form action={addTodo}>
         <input name="todo" type="text" />
         <button className="btn" type="submit">
           Add Todo
         </button>
       </form>
-
       {todos.map((todo) => (
         <div key={todo.id}>{todo.text}</div>
       ))}
