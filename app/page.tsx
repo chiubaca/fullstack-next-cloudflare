67b87d85c93d5ae4e05605e3d0c22ae9aa373540
Alex Chiu <alexchiu11@gmail.com>
2024-05-10 06:56:39 +0100

    testing...

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,20 @@
 import { drizzle } from "drizzle-orm/d1";
-import { revalidatePath } from "next/cache";
 import { getRequestContext } from "@cloudflare/next-on-pages";
 
 import { todo } from "@/schemas";
 
 export const runtime = "edge";
 
-const APP_DB = getRequestContext().env.APP_DB;
-const db = drizzle(APP_DB);
-
 export default async function Home() {
+  const APP_DB = getRequestContext().env.APP_DB;
+  const db = drizzle(APP_DB);
+
   const todos = await db.select().from(todo);
 
   async function addTodo(formData: FormData) {
     "use server";
+    const APP_DB = getRequestContext().env.APP_DB;
+    const db = drizzle(APP_DB);
 
     try {
       const formTodo = formData.get("todo") as string;
