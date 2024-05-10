379f4a5e03ce8c7b4e222a5545c2f12f8cb2966f
Alex Chiu <alexchiu11@gmail.com>
2024-05-10 20:24:37 +0100

    chore: test include actions allowed origins

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,23 @@
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 
 // Here we use the @cloudflare/next-on-pages next-dev module to allow us to use bindings during local development
 // (when running the application with `next dev`), for more information see:
 // https://github.com/cloudflare/next-on-pages/blob/5712c57ea7/internal-packages/next-dev/README.md
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === "development") {
   await setupDevPlatform();
 }
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "localhost:8788",
+        "*.fullstack-next-cloudflare.pages.dev",
+        "fullstack-next-cloudflare.pages.dev",
+      ],
+    },
+  },
+};
 
 export default nextConfig;
