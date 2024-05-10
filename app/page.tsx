d38b6db91c32819e75c859127e5e9be09f89c423
Alex Chiu <alexchiu11@gmail.com>
2024-05-10 20:50:25 +0100

    testing...

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,29 @@
-import { drizzle } from "drizzle-orm/d1";
-import { getRequestContext } from "@cloudflare/next-on-pages";
+// import { drizzle } from "drizzle-orm/d1";
+// import { getRequestContext } from "@cloudflare/next-on-pages";
 
 import { todo } from "@/schemas";
 
 export const runtime = "edge";
 
 export default async function Home() {
-  const APP_DB = getRequestContext().env.APP_DB;
-  const db = drizzle(APP_DB);
+  // const APP_DB = getRequestContext().env.APP_DB;
+  // const db = drizzle(APP_DB);
 
-  const todos = await db.select().from(todo);
+  // const todos = await db.select().from(todo);
+
+  const todos = [{ text: "test" }, { text: "test2" }];
 
   async function addTodo(formData: FormData) {
     "use server";
-    const APP_DB = getRequestContext().env.APP_DB;
-    const db = drizzle(APP_DB);
+    // const APP_DB = getRequestContext().env.APP_DB;
+    // const db = drizzle(APP_DB);
 
     try {
       const formTodo = formData.get("todo") as string;
+      console.log("🚀 ~ Home ~ formTodo:", formTodo);
 
-      await db.insert(todo).values({ text: formTodo, timestamp: Date.now() });
+      // await db.insert(todo).values({ text: formTodo, timestamp: Date.now() });
+      return formTodo;
     } catch (error) {
       console.warn("Unhandled error...", error);
     }
@@ -33,8 +37,8 @@ export default async function Home() {
           Add Todo
         </button>
       </form>
-      {todos.map((todo) => (
-        <div key={todo.id}>{todo.text}</div>
+      {todos.map((todo, idx) => (
+        <div key={idx}>{todo.text}</div>
       ))}
     </main>
   );
